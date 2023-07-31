acd1f996e1aa9430ec2f29bc22c84a0ccdd23d2c
Vikramaditya G Khupse <vikramadityakhupse@gmail.com>
2023-07-31 10:24:23 +0530

    View binding and date picker dialog box added

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.agecalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.agecalculator.databinding.ActivityMainBinding
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+            datePicker()
+        }
     }
+
+    private fun datePicker(){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,
+       { view, year, month, day ->
+
+           Toast.makeText(this, "Date Picked Successfully", Toast.LENGTH_SHORT).show()
+       },
+            year,
+            month,
+            day
+            ).show()
+
+
+    }
+
+
 }
